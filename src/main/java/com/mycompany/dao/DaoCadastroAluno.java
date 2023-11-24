4b13102f838b3c18c7f9fcec285a27fe34a88941
STEFANN� GUTIERRES DE ARAUJO <stefanny.0181@aluno.pr.senac.br>
2023-11-24 10:59:15 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/dao/DaoCadastroAluno.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dao;
+
+/**
+ *
+ * @author stefanny.0181
+ */
+public class DaoCadastroAluno {
+    
+}
---
+++ src/main/java/com/mycompany/dao/newpackage/modelo/ModEstado.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dao.newpackage.modelo;
+
+/**
+ *
+ * @author stefanny.0181
+ */
+public class ModEstado {
+    private int id;
+    private String nome;
+    private String uf;
+
+    public ModEstado(int id, int idPais, String nome, String uf) {
+        this.id = id;
+        this.nome = nome;
+        this.uf = uf;
+    }
+
+    public ModEstado() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    @Override
+    public String toString() {
+        return "ModEstado{" + "id=" + id + ", nome=" + nome + ", uf=" + uf + '}';
+    }
+    
+}
